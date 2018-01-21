cf2706d80ded220fbc75004d58a74c6aeaf2ce6b
tdhoore <timdhoore@gmail.com>
2018-01-21 15:06:15 +0100

    start html

--- src/view/events/index.php
+++ src/view/events/index.php
@@ -1,4 +1,4 @@
-<form action="index.php" method="post">
+<!--<form action="index.php" method="post">
   <fieldset>
     <legend>test</legend>
     <label for="test">
@@ -19,4 +19,79 @@
     <p class="validationComplete"></p>
     <input type="submit" name="submit" value="send"/>
   </fieldset>
-</form>
+</form>-->
+
+<section>
+  <header>
+    <h2>Ontdek wat er deze week geplanded is</h2>
+  </header>
+  <article class="">
+    <header>
+      <h3>Autovrije zondag</h3>
+      <p>16 september 2018</p>
+    </header>
+    <div class="">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque commodo ultricies nisl id accumsan. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur sit amet feugiat nulla.</p>
+      <a href="#" class="btn">Ontdek</a>
+    </div>
+    <img src="" alt=""/>
+    <img src="" alt=""/>
+  </article>
+  <article class="">
+    <header>
+      <h3>Car free day</h3>
+      <p>20 september 2018</p>
+    </header>
+    <div class="">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque commodo ultricies nisl id accumsan. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur sit amet feugiat nulla.</p>
+      <a href="#" class="btn">Ontdek</a>
+    </div>
+    <img src="" alt=""/>
+  </article>
+  <article class="">
+    <header>
+      <h3>Car free festival</h3>
+      <p>20 september 2018</p>
+    </header>
+    <div class="">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque commodo ultricies nisl id accumsan. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur sit amet feugiat nulla.</p>
+      <a href="#" class="btn">Ontdek</a>
+    </div>
+    <img src="" alt=""/>
+  </article>
+  <article class="">
+    <header>
+      <h3>Strap dag</h3>
+      <p>23 september 2018</p>
+    </header>
+    <div class="">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque commodo ultricies nisl id accumsan. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur sit amet feugiat nulla.</p>
+      <a href="#" class="btn">Ontdek</a>
+    </div>
+    <img src="" alt=""/>
+  </article>
+</section>
+<section>
+  <header>
+    <h2>Nieuws en updates</h2>
+  </header>
+  <article class="">
+    <header>
+      <h3>Title</h3>
+      <p>16 september 2018</p>
+    </header>
+    <img src="" alt=""/>
+  </article>
+  <a href="#">Meer nieuws</a>
+</section>
+
+<article class="">
+  <header>
+    <h2>#WeekvdMobiliteit</h2>
+  </header>
+  <ul>
+    <li><a href="#"><span>Facebook</span></a></li>
+    <li><a href="#"><span>Twitter</span></a></li>
+    <li><a href="#"><span>Instagram</span></a></li>
+  </ul>
+</article>
--- src/view/layout.php
+++ src/view/layout.php
@@ -2,18 +2,44 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Autovrij</title>
+    <title>Week van de mobiliteit 2018</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php echo $css;?>
   </head>
   <body>
-
-    <div class="container">
-      <?php if(!empty($_SESSION['info'])): ?><div class="alert alert-success"><?php echo $_SESSION['info'];?></div><?php endif; ?>
-      <?php if(!empty($_SESSION['error'])): ?><div class="alert alert-danger"><?php echo $_SESSION['error'];?></div><?php endif; ?>
-
-      <?php echo $content; ?>
+  <header>
+    <h1><span>Week van de mobiliteit</span></h1>
+    <nav>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Over de week</a></li>
+        <li><a href="#">Nieuws</a></li>
+        <li><a href="#">Zelf iets organiseren?</a></li>
+        <li><a href="#" class="btn">Ontdek de acties</a></li>
+      </ul>
+    </nav>
+  </header>
+  <main>
+    <?php echo $content; ?>
+  </main>
+  <footer>
+    <form class="newsLetter" action="index.html" method="post">
+      <input type="hidden" name="action" value="newsLetter"/>
+      <label for="email">
+        <span>Nieuwsbrief</span>
+        <input type="email" name="email" id="email" placeholder="name@mail.com"/>
+      </label>
+    </form>
+    <p class="copyWrite">&copy; <?php echo date("Y"); ?> Week van de mobiliteit</p>
+    <div class="socialLinks">
+        <h3>Follow us</h3>
+      <ul>
+        <li><a href="#"><span>Facebook</span></a></li>
+        <li><a href="#"><span>Twitter</span></a></li>
+        <li><a href="#"><span>Instagram</span></a></li>
+      </ul>
     </div>
-
+  </footer>
     <?php echo $js;?>
   </body>
 </html>
