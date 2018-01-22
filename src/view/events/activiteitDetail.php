c6d5621e1908a4e58fb83d6b757226926d54a0b0
tdhoore <timdhoore@gmail.com>
2018-01-22 09:16:43 +0100

    first html draft done

---
+++ src/view/events/activiteitDetail.php
@@ -0,0 +1,90 @@
+<article class="">
+  <header>
+    <h2>Autovrije zondag Sint-Niklaas: Hendrik Heymanplein</h2>
+    <p>16/09/18 - 14uur tot 18uur</p>
+    <ul class="tags">
+      <li>familie</li>
+      <li>duurzaam</li>
+      <li>eco</li>
+    </ul>
+    <div class="shareLink">
+      <p>Deel deze activiteit</p>
+      <ul>
+        <li><a href="#"><span>Facebook</span></a></li>
+        <li><a href="#"><span>Twitter</span></a></li>
+      </ul>
+    </div>
+  </header>
+  <img src="" alt=""/>
+  <p>Op zondag 17 september 2017 is het feest in de stadskern. De auto’s blijven die dag aan de kant en voetgangers en fietsers nemen de straten en pleinen in. Tientallen
+creatieve doeactiviteiten maken van autovrije zondag opnieuw een
+hoogdag van de zachte mobiliteit.</p>
+</article>
+<article class="">
+  <header>
+    <h3>Waar is dit?</h3>
+  </header>
+  <address class="">
+    Hendrik Heymanplein
+  <br/>
+  Hendrik Heymanplein
+  <br/>
+  Sint-Niklaas 9100
+  </address>
+  <a href="#">Toon op google maps</a>
+</article>
+
+<section>
+  <header>
+    <h3 class="hide">Extra information</h3>
+  </header>
+  <article class="">
+    <header>
+      <h4>Initiatie sport</h4>
+    </header>
+    <p>Maak kennis met een initiatie baseball. Of bent u eerder fan van een skateinitiatie? Misschien is curling wel iets voor u? Of houdt u eerder van een gevechts- en/of verdedigingssport? De dienst sport informeert u graag over het sportaanbod.</p>
+  </article>
+  <article class="">
+    <header>
+      <h4>Optredens</h4>
+    </header>
+    <p>Geniet even een rustpauze van de vele optredens terwijl u geniet van een drankje en een versnapering.</p>
+  </article>
+  <article class="">
+    <header>
+      <h4>Spel</h4>
+    </header>
+    <p>Daag uw vrienden uit op een rondje op de sweeper. Of ga terug in de tijd met een potje volksspelen.</p>
+  </article>
+  <article class="">
+    <header>
+      <h4>Kinderen</h4>
+    </header>
+    <p>Voor de allerkleinsten worden er ponyritjes voorzien of wat dacht u van een fietsdraaimolen? Met het treinspringkasteel gaat het vooruit. De leerlingen van de OLV-Presentatie toveren een mooie grime op uw gezicht en knutselen graag iets leuks met u.</p>
+  </article>
+  <article class="">
+    <header>
+      <h4>Opening bibliotheek</h4>
+    </header>
+    <p>In het kader van de feestelijke heropening na de renovatiewerken, is de Bib op Autovrije zondag uitzonderlijk open tot 17 uur. Een weekend vol activiteiten voor jong en oud met onder ander Begijn Le Bleu, workshops papier scheppen, buttons maken, t-shirts printen, rondleidingen, voorleesmomenten en nog veel meer.</p>
+  </article>
+  <article class="">
+    <header>
+      <h4>Zeepkistenrace</h4>
+    </header>
+    <p>Van die typische bommakasten, waar de kringloopcentra van uitpuilen omdat niemand ze nog wil, kent u die? Wel deze werden omgeturnd tot een zeepkist. Benieuwd? Neem dan zeker deel aan de race of aanschouw dit spektakel als toeschouwer.</p>
+  </article>
+</section>
+<article class="">
+  <header>
+    <h3>Practische informatie</h3>
+  </header>
+  <p> Op zondag 17 september is er van 12 tot 19 uur geen verkeer mogelijk in een groot deel van de stadskern. De afbakening van de autovrije zone vindt u op het plan. Activiteiten vinden plaats tussen 14 en 18 uur. Ter gelegenheid van autovrije zondag biedt het stadsbestuur u gratis busvervoer aan. U hoeft hiervoor niets te ondernemen,
+gewoon op de bus stappen.</p>
+<article class="">
+  <header>
+    <h4>Bekijk ook zeker onze website</h4>
+  </header>
+  <a href="https://www.sint-niklaas.be/autovrij">www.sint-niklaas.be/autovrij</a>
+</article>
+</article>
